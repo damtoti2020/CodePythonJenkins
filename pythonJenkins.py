d9bc5515a62f894e8e58cf607148f61909e68132
damtoti <richegod@gmail.com>
2022-10-30 09:01:29 +0100

    Create pythonJenkins.py

---
+++ pythonJenkins.py
@@ -0,0 +1,14 @@
+import os
+
+
+Malist1 = [9, 8, 6, 4, 1, 10, 5, 3, 2, 7]
+Malist1.reverse()
+print("The reverse list is", Malist1)
+
+List2 = [91, 67, 128, 34, 76, 78, 87, 65, 64, 345]
+List2.sort()
+print("the sorted list is ", List2)
+
+List3 = []
+List3 = Malist1.copy()
+print("Ma liste 3 = ", List3)
